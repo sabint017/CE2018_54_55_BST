f8cb29320b1b92f263d0ff01121d168b123fc6dc
Sabin Thapa <sabint017@gmail.com>
2020-01-30 09:47:33 +0545

    added bst.h

---
+++ BST.h
@@ -0,0 +1,18 @@
+//Binary search tree impementation
+// Array implementation by Sabin and LinkedList implementation by Samips
+/*
+Create a project on github and add your team member as a collaborator.
+*/
+
+
+#ifndef BST_h
+#define BST_h
+
+class BST{
+	public:
+		virtual void add(int data)=0;
+		virtual void preOrderTraversal()=0;
+		virtual void search(int datas)=0;
+};
+
+#endifs
